ac89935c7b81c8c8039c664f89947b4b5c56b155
shotaroonishi <o_onishi@kani-design.com>
2021-10-05 18:41:00 +0900

    パラパラ動きと、くるくる

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-var startPos = 0,
-  winScrollTop = 0;
-$(window).on("scroll", function () {
-  winScrollTop = $(this).scrollTop();
-  if (winScrollTop >= 200) {
-    $(".js-scroll").addClass("is-scroll");
-  } else {
-    $(".js-scroll").removeClass("is-scroll");
-  }
-  startPos = winScrollTop;
+// var startPos = 0,
+//   winScrollTop = 0;
+// $(window).on("scroll", function () {
+//   winScrollTop = $(this).scrollTop();
+//   if (winScrollTop >= 600) {
+//     $(".js-scroll").addClass("is-scroll");
+//   } else {
+//     $(".js-scroll").removeClass("is-scroll");
+//   }
+//   startPos = winScrollTop;
+// });
+
+// For more information, see greensock.com/docs/v3/Plugins/ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.create({
+  trigger: ".js-scroll",
+  markers: true,
+  start: "top center",
+  toggleClass: { targets: ".js-scroll", className: "is-scroll" },
 });
